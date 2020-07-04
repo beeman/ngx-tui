1a5c2d72a62c9add74283348809b5c3d463c0563
Bram Borggreve <borggreve@gmail.com>
2020-07-04 05:15:07 -0500

    fix: set correct build paths of tui library

--- libs/tui/jest.config.js
+++ libs/tui/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'tui',
-  preset: '../../../jest.config.js',
-  coverageDirectory: '../../../coverage/libs/tui',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/libs/tui',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
